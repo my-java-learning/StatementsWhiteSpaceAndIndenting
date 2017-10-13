f6a2982d7b28840a93a6051afd051002f3feb23b
Qsior <lukaszbojes@gmail.com>
2017-10-13 16:45:09 +0200

    Added examples

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,18 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int nextVariable = 50;
+        nextVariable--;
+        System.out.println("Another test");
+
+
     }
 }
